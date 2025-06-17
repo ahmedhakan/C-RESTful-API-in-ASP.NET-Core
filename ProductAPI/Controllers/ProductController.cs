e965e6d1e7e72bfcfe1b1c8a71d7f6fd57575809
Ahmed Hakan <126663779+ahmedhakan@users.noreply.github.com>
2025-06-17 15:54:50 +0200

    push

--- ProductAPI/Controllers/ProductController.cs
+++ ProductAPI/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ProductAPI.Models; // Assuming Product model is in this namespace
 
 namespace ProductAPI.Controllers
 {
@@ -7,5 +8,12 @@ namespace ProductAPI.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+
+        [HttpPost]
+        public IActionResult CreateProduct([FromBody] Product product)
+        {
+            // TODO: add product to db
+            return CreatedAtAction(nameof(CreateProduct), new {id = product.id}, product);
+        }
     }
 }
---
+++ ProductAPI/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace ProductAPI.Models
+{
+    public class Product
+    {
+        public  int id  { get; set; }
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
